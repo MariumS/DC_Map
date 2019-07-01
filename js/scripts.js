cd951e5b9c06da10670a6e8294b08e96899e1d5b
Marium Sultan <mas1300@nyu.edu>
2019-07-01 11:18:55 -0400

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 //personal access token, do not steal
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyemlwYW45NCIsImEiOiJjanVrOTdwaDQxdG42NDRwNGFmbzY5dWdtIn0.4lVQxPc89QYzHas2IIWmew';
 
-//create mapboxgl map, in the map container specified in css
+
 var map = new mapboxgl.Map({
-  container: 'mapContainer',
-  style: 'mapbox://styles/mapbox/dark-v10',
-  //florida center
-  center: [-73.935242,40.730610],
-  scrollWheelZoom: false,
-  scrollZoom: false,
-  zoom: 5.5,
+  container: 'map', // container element id
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-74.0059, 40.7128], // initial map center in [lon, lat]
+  zoom: 11
 });
-
-
 
 map.on('load', function() {
   map.addLayer({
-    id: 'collisions',
+    id: 'collisions
+
+    ',
     type: 'circle',
     source: {
       type: 'geojson',
