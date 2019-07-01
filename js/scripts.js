f984e03c6f128a092fd3105367471e6742e0189c
Marium Sultan <mas1300@nyu.edu>
2019-07-01 13:51:30 -0400

    beautifuy

--- js/scripts.js
+++ js/scripts.js
@@ -11,49 +11,49 @@ var map = new mapboxgl.Map({
 
 map.on('load', function() {
 
-map.addSource('DC', {
-  type: 'geojson',
-  data: './data/cleen.geojson',
-});
+  map.addSource('DC', {
+    type: 'geojson',
+    data: './data/cleen.geojson',
+  });
 
-map.addLayer({
-  'id': 'DC',
-  'type': 'circle',
-  'source': 'DC',
-  "interactive": true,
-  'paint': {
-    'circle-color': [
-      'interpolate',
-      ['linear'],
-      ['get', 'High_Risk_'],
-      //dots get slightly darker as number killed increases
-      0, '#daedf4',
-      10, '#3BB3C3',
-      20, '#669EC4',
-      30, '#8B88B6',
-      50, '#A2719B',
-      80, '#780303'
-    ],
-    'circle-opacity': 0.75,
-    'circle-radius': 4
-  }
-});
-map.on('click', 'DC', function (e) {
-new mapboxgl.Popup()
-.setLngLat(e.lngLat)
-.setHTML(`there have been ` + e.features[0].properties.High_Risk_ + ` high risk violations at ` + e.features[0].properties.DC_ID)
-.addTo(map);
-});
+  map.addLayer({
+    'id': 'DC',
+    'type': 'circle',
+    'source': 'DC',
+    "interactive": true,
+    'paint': {
+      'circle-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'High_Risk_'],
+        //dots get slightly darker as number killed increases
+        0, '#daedf4',
+        10, '#3BB3C3',
+        20, '#669EC4',
+        30, '#8B88B6',
+        50, '#A2719B',
+        80, '#780303'
+      ],
+      'circle-opacity': 0.75,
+      'circle-radius': 4
+    }
+  });
+  map.on('click', 'DC', function(e) {
+    new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML(`There have been ` + e.features[0].properties.High_Risk_ + ` high risk violations at ` + e.features[0].properties.DC_ID + `.`)
+      .addTo(map);
+  });
 
-// Change the cursor to a pointer when the mouse is over the states layer.
-map.on('mouseenter', 'DC', function () {
-map.getCanvas().style.cursor = 'pointer';
-});
+  // Change the cursor to a pointer when the mouse is over the states layer.
+  map.on('mouseenter', 'DC', function() {
+    map.getCanvas().style.cursor = 'pointer';
+  });
 
-// Change it back to a pointer when it leaves.
-map.on('mouseleave', 'DC', function () {
-map.getCanvas().style.cursor = '';
-});
+  // Change it back to a pointer when it leaves.
+  map.on('mouseleave', 'DC', function() {
+    map.getCanvas().style.cursor = '';
+  });
 
 
 
